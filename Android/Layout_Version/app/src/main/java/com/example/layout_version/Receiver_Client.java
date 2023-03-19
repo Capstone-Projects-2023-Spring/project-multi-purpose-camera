2d01708b1b4c9e4e44d2e8e9cb7d2efcfe7ae383
NickCoffin <tuo76523@temple.edu>
2023-03-19 15:23:52 -0400

    push before restarting my computer to deal with connection issues

--- Android/Layout_Version/app/src/main/java/com/example/layout_version/Receiver_Client.java
+++ Android/Layout_Version/app/src/main/java/com/example/layout_version/Receiver_Client.java
@@ -8,9 +8,44 @@ import java.io.*;
 
 public class Receiver_Client extends AppCompatActivity implements Runnable {
 
+    private Socket socket = null;
+    private DataInputStream input = null;
+    private DataOutputStream output = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Client client = new Client( ip address, port);
+    }
+    public void connect( String address, int port ){
+        try {
+            socket = new Socket(address, port);
+            input = new DataInputStream(System.in);
+            output = new DataOutputStream(socket.getOutputStream());
+        }
+        catch(UnknownHostException u){
+            System.out.println(u);
+        }
+        catch(IOException i){
+            System.out.println(i);
+        }
+        String line = "";
 
+        while(!line.equals("Over")){
+            try{
+                line = input.readLine();
+                output.writeUTF(line);
+            }
+            catch(IOException i){
+                System.out.println(i);
+            }
+        }
+        try{
+            input.close();
+            output.close();
+            socket.close();
+        }
+        catch(IOException i){
+            System.out.println(i);
+        }
     }
 }
