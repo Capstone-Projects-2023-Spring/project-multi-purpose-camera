1295a44b0bd60cb5ced24f3499bb6a4598881d3b
NickCoffin <tuo76523@temple.edu>
2023-03-19 14:37:57 -0400

    Testing that I can push stuff since it was giving me trouble last time

---
+++ Android/Layout_Version/app/src/main/java/com/example/layout_version/Receiver_Client.java
@@ -0,0 +1,16 @@
+package com.example.layout_version;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.net.*;
+import java.io.*;
+
+public class Receiver_Client extends AppCompatActivity implements Runnable {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
