6418f742e0a4a45f971a678b66afaf28f3c81fef
NickCoffin <tuo76523@temple.edu>
2023-03-23 13:47:18 -0400

    new server stuff

--- Android/Layout_Version/app/src/main/java/com/example/layout_version/Receiver_Client.java
+++ Android/Layout_Version/app/src/main/java/com/example/layout_version/Receiver_Client.java
@@ -14,23 +14,31 @@ public class Receiver_Client implements Runnable{
     //static Socket socket = new Socket(address, port);
     //static DataInputStream input = new DataInputStream(socket.getInputStream());
      //static DataOutputStream output = new DataOutputStream(socket.getOutputStream());
-    public void main(){
+    public static void main(String args []){
         //super.onCreate(savedInstanceState);
         //Client client = new Client( ip address, port);
+        System.out.println("Start of program");
         connect(address, port);
     }
     public static void connect( String address, int port) {
         try {
+            System.out.println("Start of connect method");
             DatagramSocket clientSocket = new DatagramSocket();
             InetAddress IPAddress = InetAddress.getByName(address);
+            System.out.println("checkpoint 1");
             String sentence = "R" + ID;
             byte[] bytes = sentence.getBytes(StandardCharsets.UTF_8);
             byte[] receivebytes = sentence.getBytes(StandardCharsets.UTF_8);
             DatagramPacket sendPacket = new DatagramPacket(bytes, bytes.length, IPAddress, port);
+            System.out.println("checkpoint 2");
             clientSocket.send(sendPacket);
+            System.out.println("checkpoint 3");
             DatagramPacket receivePacket = new DatagramPacket(receivebytes, receivebytes.length);
+            System.out.println("checkpoint 4");
             clientSocket.receive(receivePacket);
+            System.out.println("checkpoint 5");
             receivebytes = receivePacket.getData();
+            System.out.println("checkpoint 6");
             String decoded = new String(receivebytes, "UTF-8");
             System.out.println("From server: " + decoded);
             clientSocket.close();
