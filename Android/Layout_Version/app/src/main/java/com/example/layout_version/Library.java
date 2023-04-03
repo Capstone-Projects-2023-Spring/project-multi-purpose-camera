a966808a0a7b084fd5ae53a975e8ffac3894fe30
JustinArd <tuo59359@temple.edu>
2023-04-03 01:37:51 -0400

    Added a reusable Library layout for videos, Library WIP

--- Android/Layout_Version/app/src/main/java/com/example/layout_version/Library.java
+++ Android/Layout_Version/app/src/main/java/com/example/layout_version/Library.java
@@ -1,12 +1,20 @@
 package com.example.layout_version;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.MediaController;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -51,5 +59,47 @@ public class Library extends AppCompatActivity {
             }
         });
 
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        RelativeLayout container = findViewById(R.id.captured_videos);
+        int marg = 0;
+
+        ViewGroup myLayout = (ViewGroup) LayoutInflater.from(this).inflate(R.layout.library_videos, null);
+        container.addView(myLayout);
+
+        ViewGroup myLayout2 = (ViewGroup) LayoutInflater.from(this).inflate(R.layout.library_videos, null);
+        params.setMargins(0, 230, 0, 0);
+        myLayout2.setLayoutParams(params);
+        container.addView(myLayout2);
+
+        ViewGroup myLayout3 = (ViewGroup) LayoutInflater.from(this).inflate(R.layout.library_videos, null);
+        params.setMargins(0, 460, 0, 0);
+        myLayout3.setLayoutParams(params);
+        container.addView(myLayout3);
+
+//        VideoView videoView = new VideoView(this);
+//        MediaController mediaController = new MediaController (this);
+//        videoView.setMediaController(mediaController);
+//        mediaController.setAnchorView(videoView);
+//        Uri uri = Uri.parse("https://mpc-capstone.s3.amazonaws.com/gatoilse-cat-love.mp4");
+//        videoView.setVideoURI(uri);
+//        videoView.requestFocus();
+//        videoView.start();
+//        myLayout.addView(videoView);
+
+
+//        for (int i = 0; i < 5; i++)
+//        {
+//            ViewGroup myLayout = (ViewGroup) LayoutInflater.from(this).inflate(R.layout.library_videos, null);
+//            params.setMargins(0, marg, 0, 0);
+//            myLayout.setLayoutParams(params);
+//            container.addView(myLayout);
+//            params.setMargins(0, marg+230, 0, 0);
+//        }
+
     }
+
 }
