bd89d0ccee3bccd649122848a0836440d1030710
Tuk12915 <Tuk12915@temple.edu>
2023-04-17 07:48:49 -0400

    updated but still not working properly

--- Raspberry Pi/bluetooth_parser.py
+++ Raspberry Pi/bluetooth_parser.py
@@ -10,7 +10,31 @@ def wifi_setup(ssid,password):
     # path for wpa_supplicant configuration file 
     wpa_supplicant_path = "/etc/wpa_supplicant/wpa_supplicant.conf"
     
-    subprocess.run(f'echo -e "network={{\\n  ssid=\\"{ssid}\\"\\n  psk=\\"{password}\\"\\n}}" >> /etc/wpa_supplicant/wpa_supplicant.conf', shell=True)
+#     cmd = "sudo rm '/home/mpc/Capstone/project-multi-purpose-camera/Raspberry Pi/wpa_supplicant.conf.temp'"
+#     #command = "sudo echo -e network={{\\n  ssid=\\"{ssid}\\"\\n  psk=\\"{password}\\"\\n}}" >> "/home/mpc/Capstone/project-multi-purpose-camera/Raspberry Pi/wpa_supplicant.conf""
+#     
+#     subprocess.run(cmd, shell=True)
+    
+    """Writes new wifi info to a text file"""
+    with open("/home/mpc/Capstone/project-multi-purpose-camera/Raspberry Pi/temp.txt", "a+") as wifi:
+        wifi.write("network={\n")
+        wifi.write('\tssid="%s"\n'%ssid)
+        wifi.write('\tpsk="%s"\n'%password)
+        wifi.write('\tkey_mgmt=WPA-PSK\n')
+        wifi.write("}\n")
+        
+    """copies text file to a temp wpa_supplicant"""
+    cmd = "sudo cp temp.txt wpa_supplicant.conf.temp"
+    subprocess.run(cmd,shell=True)
+    
+    """copies the temp wpa_supplicant to the actual wpa_supplicant that the pi will use"""
+    cmd = "sudo cp wpa_supplicant.conf.temp /etc/wpa_supplicant/wpa_supplicant.conf"
+    subprocess.run(cmd,shell=True)
+    
+    cmd = "sudo reboot"
+    subprocess.run(cmd,shell=True)
+    
+    
 
 def wifi_parser(byte_string):
     string_list = byte_string.decode().split('\n')
@@ -18,12 +42,14 @@ def wifi_parser(byte_string):
     ssid = string_list[1:2]
     password = string_list[2:3]
     
-    print(ssid)
-    print(password)
-    wifi_setup(ssid,password)
+    string_ssid = ''.join(ssid)
+    string_password = ''.join(password)
+#     print(string_ssid)
+#     print(string_password)
+    wifi_setup(string_ssid,string_password)
     
     
    
 if __name__ == "__main__":
-    byte_string = b'wifi\nname\npassword\n'
+    byte_string = b'wifi\nCOMPUTER 7084\nAC24dc34!\n'
     wifi_parser(byte_string)
