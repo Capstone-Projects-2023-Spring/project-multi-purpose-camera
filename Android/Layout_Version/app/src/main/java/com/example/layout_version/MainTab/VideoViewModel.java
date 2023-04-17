f822457df109bb381c459a6406676b06ae7004c8
Keita Nakashima <tun05036@temple.edu>
2023-04-17 00:48:38 -0400

    Wording fix

--- Android/Layout_Version/app/src/main/java/com/example/layout_version/MainTab/VideoViewModel.java
+++ Android/Layout_Version/app/src/main/java/com/example/layout_version/MainTab/VideoViewModel.java
@@ -20,7 +20,7 @@ public class VideoViewModel extends ViewModel {
         updateFlag = new MutableLiveData<>(0);
         token = new MutableLiveData<>(null);
         videos = new ArrayList<>();
-        videos.add(new VideoItem("Welcome to the Video Library", "Sign in first to view the list of videos"));
+        videos.add(new VideoItem("Welcome to the Video Library", "Sign in first to view a list of videos"));
         Log.e("LiveViewModel Created", "LiveViewModel Created");
     }
 
