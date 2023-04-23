7a735f52d108860dc5037f42c8bc4b0af1fd3e18
Keita Nakashima <tun05036@temple.edu>
2023-04-23 04:26:32 -0400

    Thumbnail added to video library

--- Android/Layout_Version/app/src/main/java/com/example/layout_version/MainTab/Library/LibraryFragmentInterface.java
+++ Android/Layout_Version/app/src/main/java/com/example/layout_version/MainTab/Library/LibraryFragmentInterface.java
@@ -28,9 +28,9 @@ public interface LibraryFragmentInterface {
                 .mapToObj(i -> {
                     try {
                         JSONObject item = jsonArray.getJSONObject(i);
-                        return new VideoItem(item.get("file_name").toString(), item.get("timestamp").toString(), item.getString("url"));
+                        return new VideoItem(item.get("file_name").toString(), item.get("timestamp").toString(), item.getString("url"), item.getString("thumbnail"));
                     } catch (JSONException e) {
-                        return new VideoItem("Unknown Video", "Failed to retrieve video file", null);
+                        return new VideoItem("Unknown Video", "Failed to retrieve video file", null, null);
                     }
                 })
                 .collect(Collectors.toList());
--- Android/Layout_Version/app/src/main/java/com/example/layout_version/MainTab/Library/VideoItem.java
+++ Android/Layout_Version/app/src/main/java/com/example/layout_version/MainTab/Library/VideoItem.java
@@ -5,13 +5,15 @@ public class VideoItem {
     private final String description;
 
     private final String url;
+    private final String thumbnailUrl;
 
-    public static final VideoItem DEFAULT_VIDEO_ITEM = new VideoItem("Welcome to the Video Library", "Sign in first to view a list of videos", null);
+    public static final VideoItem DEFAULT_VIDEO_ITEM = new VideoItem("Welcome to the Video Library", "Sign in first to view a list of videos", null, null);
 
-    public VideoItem(String title, String description, String url) {
+    public VideoItem(String title, String description, String url, String thumbnailUrl) {
         this.title = title;
         this.description = description;
         this.url = url;
+        this.thumbnailUrl = thumbnailUrl;
     }
 
     public String getTitle() {
@@ -23,5 +25,9 @@ public class VideoItem {
     }
     public String getUrl() {
         return url;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
     }
 }
