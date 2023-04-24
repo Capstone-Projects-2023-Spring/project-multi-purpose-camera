866639ef38ba9496e904cc95ad7299560a71f938
NickCoffin <tuo76523@temple.edu>
2023-04-24 16:49:26 -0400

    delete unused imports

--- Android/Layout_Version/app/src/main/java/com/example/layout_version/Notifications.java
+++ Android/Layout_Version/app/src/main/java/com/example/layout_version/Notifications.java
@@ -1,52 +1,11 @@
 package com.example.layout_version;
 
-import static android.app.PendingIntent.getActivity;
-import static androidx.core.content.ContextCompat.getSystemService;
-
-import static java.security.AccessController.getContext;
-
 import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.content.BroadcastReceiver;
-import android.content.ComponentName;
-import android.content.ContentResolver;
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.IntentSender;
-import android.content.ServiceConnection;
-import android.content.SharedPreferences;
-import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
-import android.content.res.AssetManager;
-import android.content.res.Configuration;
-import android.content.res.Resources;
-import android.database.DatabaseErrorHandler;
-import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
-import android.os.Build;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.UserHandle;
-import android.view.Display;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.security.AccessController;
 
 public class Notifications {
     private Context context;
