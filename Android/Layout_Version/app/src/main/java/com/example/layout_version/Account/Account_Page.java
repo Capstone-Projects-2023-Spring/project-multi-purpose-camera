b819b84ad59ba4be65f426f2c961cb5ebc894225
tuj04825 <tuj04825@temple.edu>
2023-04-29 12:39:36 -0400

    new home page
    Basic design of initial page

--- Android/Layout_Version/app/src/main/java/com/example/layout_version/Account/Account_Page.java
+++ Android/Layout_Version/app/src/main/java/com/example/layout_version/Account/Account_Page.java
@@ -3,13 +3,13 @@ package com.example.layout_version.Account;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.TextView;
+//import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationManagerCompat;
+//import androidx.core.app.NotificationManagerCompat;
 
 import com.example.layout_version.MainTab.Library.VideoViewModel;
-import com.example.layout_version.Notifications;
+//import com.example.layout_version.Notifications;
 import com.example.layout_version.R;
 
 public class Account_Page extends AppCompatActivity {
@@ -21,35 +21,20 @@ public class Account_Page extends AppCompatActivity {
         setContentView(R.layout.account_login_signup_page);
         account = Account.getInstance();
 
-        TextView username = findViewById(R.id.username);
-        TextView password = findViewById(R.id.password);
-
         Button loginbtn = findViewById(R.id.login);
         Button signupbtn = findViewById(R.id.signup);
 
-        TextView resetPassword = findViewById(R.id.resetPassword);
-        Notifications notif = new Notifications(this);
-        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
-        notif.send_Sign_In_Notification(managerCompat);
-        loginbtn.setOnClickListener(v ->
-                account.signin(
-                        Account_Page.this,
-                        username.getText().toString(),
-                        password.getText().toString(),
-                        () -> onBackPressed(),
-                        () -> {}
-                )
-        );
+
+        loginbtn.setOnClickListener(view -> {
+            startActivity(new Intent (Account_Page.this,Account_Page_Login.class));
+        });
+
 //        loginbtn.setOnClickListener(view -> {
 //            startActivity(new Intent (Account_Page.this, Bluetooth_Page.class));
 //        });
 
         signupbtn.setOnClickListener(view -> {
             startActivity(new Intent (Account_Page.this,Account_Page_Signup.class));
-        });
-
-        resetPassword.setOnClickListener(view -> {
-            startActivity(new Intent (Account_Page.this,Account_Page_Forgot_Password.class));
         });
 
     }
